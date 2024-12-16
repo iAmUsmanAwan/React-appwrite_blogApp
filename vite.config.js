f37f95fea2a8f7ad2460eacf72db098904cb16dc
Admiral General <muhammadusmanawan88@gmail.com>
2024-12-16 19:19:39 +0500

    Uploading to netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-appwrite_blogApp/',
+  base: '/React-appwrite_blogApp/',   // Set to the deployment subdirectory
 });
